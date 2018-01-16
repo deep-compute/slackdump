e2163545ea9cfe323b27a9dc1999a351f619bed4
Umang Thapliyal <UmangThapliyal>
2018-01-16 19:46:40 +0530

    Get slack data using slack api

--- slack_dump/slack_dump.py
+++ slack_dump/slack_dump.py
@@ -14,42 +14,43 @@ CONNECTION_SLEEP_INTERVAL = 1
 EMPTY_READ_SLEEP_INTERVAL = .1
 
 def initialise(auth_token):
-	token = auth_token
-	if collection.count() == 0:
-		start1 = 0
-	else:
-		res = db.slack_db.find().sort('ts',pymongo.DESCENDING).limit(1)
-		for r in res:
-			start1 = r['ts']
-	get_real_time_message(token, start1)
+    token = auth_token
+    if collection.count() == 0:
+        start1 = 0
+    else:
+        res = db.slack_db.find().sort('ts',pymongo.DESCENDING).limit(1)
+	for r in res:
+            start1 = r['ts']
+    get_real_time_message(token, start1)
 
 def get_real_time_message(token, start1):
-	try:
-		slack = SlackClient(token)
-		while not slack.rtm_connect():
-			time.sleep(CONNECTION_SLEEP_INTERVAL)
-		end = format(time.time(), '0.6f')
+    try:
+        slack = SlackClient(token)
+        while not slack.rtm_connect():
+            time.sleep(CONNECTION_SLEEP_INTERVAL)
+        end = format(time.time(), '0.6f')
 
-		slack_history.initialise(token,start1,end)
+        slack_history.initialise(token,start1,end)
 
-		while True:
-			msg = slack.rtm_read()
-			if not msg:
-				time.sleep(EMPTY_READ_SLEEP_INTERVAL)
-				continue
-			print (msg)
-			if not len(msg[0]) == 1:
-				try:
-					key = hashlib.sha1(msg[0]['channel'] + msg[0]['ts']).hexdigest()
-					msg[0]["key"] = key
-					res=db.slack_db.insert_one(msg[0])
-					print "Inserted!!"
+        while True:
+            msg = slack.rtm_read()
+            if not msg:
+                time.sleep(EMPTY_READ_SLEEP_INTERVAL)
+                continue
+	    print (msg)
 
-				except Exception:
-					continue
+            if not len(msg[0]) == 1:
+                try:
+                    key = hashlib.sha1(msg[0]['channel'] + msg[0]['ts']).hexdigest()
+		    msg[0]["key"] = key
+		    res=db.slack_db.insert_one(msg[0])
+		    print "Inserted!!"
 
-        except Exception as e:
-			print e
-			start1 = format(time.time(), '0.6f')
-			print "Connection failed at : "+str(start1)
-			get_real_time_message(token,start1)
+		except Exception:
+                    continue
+
+    except Exception as e:
+        print e
+	start1 = format(time.time(), '0.6f')
+	print "Connection failed at : "+str(start1)
+	get_real_time_message(token,start1)
