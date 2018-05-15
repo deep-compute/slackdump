9c9fab3df4733591f7c97a441e435a5e87f7e6eb
ManishaBayya <manisha@deepcompute.com>
2018-05-15 17:22:56 +0530

    added testcases

--- slackdump/slackhistory.py
+++ slackdump/slackhistory.py
@@ -144,6 +144,15 @@ class SlackHistory(object):
         return msg
 
     def change_status(self, _id, ts):
+        '''
+	>>> ob = SlackHistory()
+        >>> ob.change_status("U8S2NSX6J", "1518582605.000239")
+        >>> DiskDict( "/tmp/disk.dict")["U8S2NSX6J_oldest"]
+        '1518582605.000239'
+        >>> ob.change_status("U8S2NSX6J", "1518582705.000239")
+        >>> DiskDict( "/tmp/disk.dict")["U8S2NSX6J_latest"]
+        '1518582705.000239'
+        '''
         if _id + '_oldest' in self.dd.keys() and ts > self.dd[_id + '_oldest']:
             self.dd[_id + '_latest'] = ts
         else:
